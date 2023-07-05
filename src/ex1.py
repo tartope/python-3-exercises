7a41e053aadbad0992496b194a1d511b736638a3
Tunisia Artope <tartope@xpanxion.com>
2023-07-05 19:37:55 -0400

    Complete ex1.

--- src/ex1.py
+++ src/ex1.py
@@ -1,14 +1,28 @@
 from ValidationException import ValidationException
 
 
-def validate_file():
-    pass # TODO
+def validate_file(file_name):
+    with open(file_name, "r") as file:
+        next(file)
+        # for count, line in enumerate(file):
+        #     line_values = line.split(',')
+        #     # if second value is number -> throw exceptions
+        #     try:
+        #         int(line_values[1])
+        #     except:
+        #         raise ValidationException(f"Invalid mileage: {line_values[1]}")
+
+        for line in file:
+            try:
+                int(line[3:])
+            except:
+                raise ValidationException(f"Invalid mileage: {line[3:]}")
 
 
 
 def ex1():
     try:
-        validate_file("input.txt")
+        validate_file("../files/input.txt")
     except ValidationException as ve:
         print(ve)
 
